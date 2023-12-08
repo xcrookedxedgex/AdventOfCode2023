4d40f20ada3f978245082f1d0821167ae76a2914
xcrookedxedgex <cedricpurtschert@gmail.com>
2023-12-08 18:01:27 +0100

    some changes

--- DayTwo/main.py
+++ DayTwo/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello")
+def adventOfCodeDayTwo():
+    inputString = read_text_file()
+    print(inputString)
+
+def read_text_file():
+    inputLines = ""
+    #reads a .txt file and returns the content as list
+    with open('input.txt') as f:
+        inputLines = f.readlines()
+    return inputLines
 
 
 if __name__ == "__main__":
-    main()
+    adventOfCodeDayTwo()
