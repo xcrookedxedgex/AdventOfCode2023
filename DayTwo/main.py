b58757e248fffe25ddf1d3551e286e75ead48002
xcrookedxedgex <cedricpurtschert@gmail.com>
2023-12-08 18:40:53 +0100

    added more functions

--- DayTwo/main.py
+++ DayTwo/main.py
@@ -1,6 +1,7 @@
 def adventOfCodeDayTwo():
     inputString = read_text_file()
-    print(inputString)
+    input_dict = create_directory(inputString)
+    print(input_dict)
 
 def read_text_file():
     inputLines = ""
@@ -8,6 +9,17 @@ def read_text_file():
     with open('input.txt') as f:
         inputLines = f.readlines()
     return inputLines
+
+def create_directory(games):
+    input_dict = {}
+    for game in games:
+        key, value = game.split(":")
+        key = key.strip()
+        value.strip()
+        key = int(''.join(filter(str.isdigit, key))) #searches for the first appearance of a digit in a string
+        input_dict[key] = value
+    return input_dict
+
 
 
 if __name__ == "__main__":
